49ecc739412503a9d9c611e21f759c7c6971388d
Jimmy Xu <xujimmyy@gmail.com>
2017-03-04 21:28:47 -0500

    animation attempt

--- src/main/java/moodify/moodify/MainMenu.java
+++ src/main/java/moodify/moodify/MainMenu.java
@@ -21,42 +21,7 @@ import java.io.InputStream;
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainMenu extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
     private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
@@ -69,27 +34,6 @@ public class MainMenu extends AppCompatActivity {
             mControlsView.setVisibility(View.VISIBLE);
         }
     };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-//            if (AUTO_HIDE) {
-//                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-//            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,55 +41,29 @@ public class MainMenu extends AppCompatActivity {
 
         setContentView(R.layout.activity_main_menu);
 
-        mVisible = false;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+//        InputStream stream = null;
+//        try {
+//            stream = getAssets().open("colourCircle.gif");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+//        WebView webviewActionView;
+//
+//        webviewActionView = (WebView)findViewById(R.id.webviewActionView);
+//        webviewActionView.setWebViewClient(new MainMenu.MyWebViewClient());
+//        webviewActionView.getSettings().setJavaScriptEnabled(true);
+//
+//        GifWebView view = new GifWebView(this, stream);
+//        webviewActionView.addView(view);
+//        WebView webviewActionView=(WebView)findViewById(R.id.webviewActionView);
+//        webviewActionView.loadUrl("colourCircle.html");
     }
 
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    public class MainActivity extends Activity {
-
-        WebView webviewActionView;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_menu);
-
-            InputStream stream = null;
-            try {
-                stream = getAssets().open("colourCircle.gif");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            webviewActionView = (WebView)findViewById(R.id.webviewActionView);
-            webviewActionView.setWebViewClient(new MyWebViewClient());
-            webviewActionView.getSettings().setJavaScriptEnabled(true);
-
-            GifWebView view = new GifWebView(this, stream);
-            webviewActionView.addView(view);
-        }
-
-        private class MyWebViewClient extends WebViewClient {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
         }
     }
 }
