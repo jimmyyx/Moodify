cd519833709ade3dc85f2f808ff9555a48a6cea1
Jimmy Xu <xujimmyy@gmail.com>
2017-03-05 01:10:21 -0500

    Add files via upload

---
+++ MainMenu.java
@@ -0,0 +1,19 @@
+package moodify.moodify;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_menu);
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.imageDisplay);
+        gifImageView.setGifImageResource(R.drawable.colourcircle);
+    }
+}
